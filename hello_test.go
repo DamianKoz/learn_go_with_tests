9d7fdb75f35493acab779ae2e68da7593fb71761
Damian <damian.makoza@gmail.com>
2022-08-11 16:36:16 +0200

    Refactored first Test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jack")
-	want := "Hello, Jack"
-
-	if got != want {
-		t.Errorf("got: %q, want: %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
 	}
+	t.Run("Name is given", func(t *testing.T) {
+		got := Hello("Jack")
+		want := "Hello, Jack"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("No Name given", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
